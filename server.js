8f7257b8446adc0e687516917d2dc638002ecccf
cole trammer <cole trammer>
2017-05-24 18:47:26 -0700

    added the file size and form

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const multer = require("multer")({dest: "data/"});
+const path = require("path");
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("index");
+});
+
+app.post("/", multer.single("file"), (req, res) => {
+    if (req.file){
+        res.send({size: req.file.size});
+    } else {
+        res.send({error: "no file submitted"});
+    }
 });
 
 app.listen(app.get("port"), () => {
